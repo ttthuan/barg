929b43a2ba7f90159a1ff58db29af355af31e5ae
ttthuan <ttthuan001@gmail.com>
2017-12-29 16:40:19 +0700

    change data

--- AppDinhViToaDoKhach/src/components/Customers.vue
+++ AppDinhViToaDoKhach/src/components/Customers.vue
@@ -4,7 +4,7 @@
       <h4 style="color: #FFF; line-height: 2.5;">DANH SÁCH KHÁCH HÀNG</h4>
     </div>
     <div id="list-area-content">
-      <div class="list-item noselect" v-for="customer in customers">
+      <div class="list-item noselect" v-for="request in requests">
         <div class="item-avatar">
           <div class="avatar">
             <div class="avatar-text">t</div>
@@ -12,18 +12,18 @@
         </div>
         <div class="item-content">
           <div class="item-name">
-            Thành tró điên
+            trinh thanh thuan
           </div>
           <div class="item-start">
             <img :src="linkStartPoint" class="item-image">
             <div class="item-start-content">
-              sai gon
+              {{request.val().address}}
             </div>
           </div>
           <div class="item-phone">
             <img :src="linkPhone" class="item-image">
             <div class="item-phone-content">
-              {{customer.key}}
+              {{request.val().customerid}}
             </div>
           </div>
         </div>
@@ -42,18 +42,18 @@ export default {
       linkStartPoint: "../../src/assets/image/start-point.png",
       linkStopPoint: "../../src/assets/image/end-point.png",
       linkPhone: "../../src/assets/image/phone.png",
-      customers: []
+      requests: []
     }
   },
   mounted(){
     var self = this;
-    var database = firebase.database().ref('customers');
+    var database = firebase.database().ref('requests');
     database.once('value', function(snapshot){
         snapshot.forEach(function(childSnapshot) {
           //var childKey = childSnapshot.key; get key
           //var childData = childSnapshot.val(); // get data
-          self.customers.push(childSnapshot);
-          console.log(childSnapshot.child());
+          self.requests.push(childSnapshot);
+          console.log(childSnapshot.val());
         });
     });
   },
