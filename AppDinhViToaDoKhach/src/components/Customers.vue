3dc1d48ddd1b1a03db43dacb0ae6d641927bed77
ttthuan <ttthuan001@gmail.com>
2017-12-29 15:49:51 +0700

    fix bug

--- AppDinhViToaDoKhach/src/components/Customers.vue
+++ AppDinhViToaDoKhach/src/components/Customers.vue
@@ -4,10 +4,10 @@
       <h4 style="color: #FFF; line-height: 2.5;">DANH SÁCH KHÁCH HÀNG</h4>
     </div>
     <div id="list-area-content">
-      <div class="list-item noselect">
+      <div class="list-item noselect" v-for="customer in customers">
         <div class="item-avatar">
           <div class="avatar">
-            <div class="avatar-text">T</div>
+            <div class="avatar-text">t</div>
           </div>
         </div>
         <div class="item-content">
@@ -15,21 +15,15 @@
             Thành tró điên
           </div>
           <div class="item-start">
-            <img src="#" class="item-image">
+            <img :src="linkStartPoint" class="item-image">
             <div class="item-start-content">
-              Sài Gòn
-            </div>
-          </div>
-          <div class="item-end">
-            <img src="#" class="item-image">
-            <div class="item-end-content">
-              Gia Lai
+              sai gon
             </div>
           </div>
           <div class="item-phone">
-            <img src="#" class="item-image">
+            <img :src="linkPhone" class="item-image">
             <div class="item-phone-content">
-              0123456789
+              {{customer.key}}
             </div>
           </div>
         </div>
@@ -39,12 +33,29 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 
 export default {
   name: 'Customers',
   data () {
     return {
+      linkStartPoint: "../../src/assets/image/start-point.png",
+      linkStopPoint: "../../src/assets/image/end-point.png",
+      linkPhone: "../../src/assets/image/phone.png",
+      customers: []
     }
+  },
+  mounted(){
+    var self = this;
+    var database = firebase.database().ref('customers');
+    database.once('value', function(snapshot){
+        snapshot.forEach(function(childSnapshot) {
+          //var childKey = childSnapshot.key; get key
+          //var childData = childSnapshot.val(); // get data
+          self.customers.push(childSnapshot);
+          console.log(childSnapshot.child());
+        });
+    });
   },
   methods:{
   }
