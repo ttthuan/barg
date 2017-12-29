34a266381a6525689298dc37eb00f5dd45215829
ttthuan <ttthuan001@gmail.com>
2017-12-29 14:59:14 +0700

    app dinh vi khach hang

---
+++ AppDinhViToaDoKhach/src/components/Customers.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="list-area">
+    <div id="list-area-header" class="noselect">
+      <h4 style="color: #FFF; line-height: 2.5;">DANH SÁCH KHÁCH HÀNG</h4>
+    </div>
+    <div id="list-area-content">
+      <div class="list-item noselect">
+        <div class="item-avatar">
+          <div class="avatar">
+            <div class="avatar-text">T</div>
+          </div>
+        </div>
+        <div class="item-content">
+          <div class="item-name">
+            Thành tró điên
+          </div>
+          <div class="item-start">
+            <img src="#" class="item-image">
+            <div class="item-start-content">
+              Sài Gòn
+            </div>
+          </div>
+          <div class="item-end">
+            <img src="#" class="item-image">
+            <div class="item-end-content">
+              Gia Lai
+            </div>
+          </div>
+          <div class="item-phone">
+            <img src="#" class="item-image">
+            <div class="item-phone-content">
+              0123456789
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Customers',
+  data () {
+    return {
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
---
+++ AppDinhViToaDoKhach/src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <span>{{smg}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      smg: 'Here is index',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
