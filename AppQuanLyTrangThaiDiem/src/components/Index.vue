9fcb7d0c03c59177db3b91c0ae56be0f81888469
1642027 <chua.anime@gmail.com>
2018-01-04 18:50:10 +0900

    Update

---
+++ AppQuanLyTrangThaiDiem/src/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      smg: 'Here is index',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- AppQuanLyTrangThaiDiem/src/main.js
+++ AppQuanLyTrangThaiDiem/src/main.js
@@ -1,15 +1,45 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import firebase from 'firebase';
+
+// main compoment
 import App from './App'
-import router from './router'
+
+// router components
+import Index from './components/Index.vue';
+import Login from './components/Login.vue';
+
+Vue.use(VueRouter);
+
+// Initialize Firebase
+var config = {
+   apiKey: "AIzaSyAARBsG4HnBEqtCGFTDpR-BL7HLmBKD7TQ",
+   authDomain: "barg-firebase.firebaseapp.com",
+   databaseURL: "https://barg-firebase.firebaseio.com",
+   projectId: "barg-firebase",
+   storageBucket: "barg-firebase.appspot.com",
+   messagingSenderId: "573103357657"
+};
+firebase.initializeApp(config);
+
+//router
+const routes = [
+	{ path: '/', component: Index },
+	{ path: '/login', component: Login }
+];
+
+const router = new VueRouter({
+	routes
+});
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
   components: { App }
 })
